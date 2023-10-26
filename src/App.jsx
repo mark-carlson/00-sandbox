8766613c0662fc8cb02596852d2714c0fc088917
Mark Carlson <mcarlson@avenuecode.com>
2023-10-25 20:02:32 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import HelloWorld, { GoodbyeWorld } from './HelloWorld';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hi Everyone At UC Berkeley</h1>
+      <HelloWorld />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <GoodbyeWorld />
+    </>
+  );
+}
+
+export default App;
---
+++ src/HelloWorld.jsx
@@ -0,0 +1,15 @@
+export default function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export const GoodbyeWorld = () => {
+  return (
+    <div>
+      <h1>Goodbye World</h1>
+    </div>
+  );
+};
